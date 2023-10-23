df70bbb1f40f60dca822a2f7c1f052d2977468c6
ErnstOsbeck <143172129+ErnstOsbeck@users.noreply.github.com>
2023-10-23 20:09:42 +0200

    Add project files.

---
+++ Inlämningsuppgift 4.9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace uppgift_4._9
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur lång vill du att triangelns sida ska vara? svara med en siffra.");
+            int längd = int.Parse(Console.ReadLine());
+            for (int i = 0; i < längd; i++)
+            {
+                for (int j = 0; j < i + 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            Console.Read();
+        }
+    }
+}
